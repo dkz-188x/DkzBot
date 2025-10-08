01047c22564f434b3f9908acad6ed3ee3f7bc5b3
dkz-188x <dhanykece11@gmail.com>
2025-10-08 17:48:31 +0700

    Create menu.js

---
+++ cmd/menu.js
@@ -0,0 +1,75 @@
+module.exports = [
+  {
+    name: "menu",
+    alias: ["help"],
+    desc: "Menampilkan semua menu bot",
+    run: async (m, { conn }) => {
+      let user = m.pushName || "User"
+      let teks = `
+─Hai, ${user}👋
+
+╭──── 「 *👤USER INFO* 」
+│
+│└❑ Status : 
+│└❑ Limit : 
+│└❑ Level : 
+╰────────────────
+
+╭──「 *OWNER* 」
+│    • .addprem <nomor>
+│    • .delprem <nomor>
+│    • .resetlimit
+│    • .ban <nomor>
+│    • .unban <nomor>
+│    • .self
+│    • .public
+│    • .joingc <link>
+│    • .out
+│    • .setthumbnail <link>
+╰────────────────
+
+╭─「 *FUN* 」
+│    • .brat
+│    • .bratvid
+│    • .tebakkata
+│    • .qc1
+│    • .qc2
+│    • .s
+│    • .smeme
+│    • .cekprofile <@user>
+╰────────────────
+
+╭─「 *RPG* 」
+│    • .rvo ®
+│    • .me
+│    • .limit
+│    • .ceklimit <@user>
+╰────────────────
+
+╭─「 *DOWNLOADER* 」
+│    • .yt <link>
+│    • .ytmp3 <link> 
+│    • .tt <link>
+│    • .ttmp3 <link>
+│    • .tovid
+│    • .tomp3
+╰────────────────
+
+╭─「 *GROUP* 」
+│    • .tagall ®
+│    • .hidetag ®
+│    • .kick <reply> ®
+│    • .add <nomor> ®
+│    • .open ®
+│    • .close ®
+│    • .getpp <reply>
+│    • .listonline 
+│    • .totalchat
+│    • .afk
+│    • .antilink ®
+│    • .antilink off ®
+│    • .linkgc ®
+╰────────────────
+® = Hanya admin yg bisa menggunakan fitur ini!
+`
+      await conn.sendMessage(m.chat, { text:
