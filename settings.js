340b882994772c5bf0b25b4522dc6333ad211307
dkz-188x <dhanykece11@gmail.com>
2025-10-08 15:18:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ module.exports = {
         mode: { public: true },       // true = public, false = self
         ownerNumber: '6283119404224',    // ganti dengan nomor WA owner
         ownerName: 'Dkz🗿',       // ganti dengan nama owner
-        premiumUsers: [20],             // nomor premium
-        userLimits: {20},               // limit user
+        premiumUsers: [],             // nomor premium
+        userLimits: {},               // limit user
         botThumbnail: null            // thumbnail bot, bisa diganti via .addthumbnail
     },
     botInfo: {
