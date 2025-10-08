90c6ff0d5f570634dfb1a3d81cc4eb2828f8b197
dkz-188x <dhanykece11@gmail.com>
2025-10-08 15:38:13 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 module.exports = {
     global: {
-        mode: { public: true },          // true = public, false = self
-        ownerNumber: '6283119404224',       // ganti dengan nomor WA owner
-        ownerName: 'Dkz',         // ganti dengan nama owner
-        premiumUsers: ['6283120983152'],    // nomor premium default
-        userLimits: 20,                  // limit user default
-        botThumbnail: null               // thumbnail default (null artinya belum ada)
+        mode: { public: true },           // true = public, false = self
+        ownerName: 'Dkz',                 // nama owner
+        ownerNumber: '6283119404224',        // nomor WA owner
+        premiumUsers: [],                  // bisa dikosongkan
+        userLimits: 20,                    // limit default user
+        botThumbnail: null                 // thumbnail default kosong, bisa diganti manual
     },
     botInfo: {
         name: 'DkzBot',
